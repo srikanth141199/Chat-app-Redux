8d87b261167d56e25e6b0527d4288094d308e747
Srikanth K <kolleparasrikanth@gmail.com>
2024-05-26 21:51:56 +0530

    Messages implemented

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -1,9 +1,40 @@
+import { useEffect, useRef } from "react";
 import "./Message.css"
+import { useSelector } from "react-redux";
+import { chatSelector } from "../../redux/Reducers/chatReducer";
+import { authSelector } from "../../redux/Reducers/authReducer";
 
-export default function Message(){
-    return(
-        <div className="messageContainer">
-            Message
-        </div>
-    )
+export default function Message({message}){
+
+    const {user} = useSelector(chatSelector);
+    const {userDetails} = useSelector(authSelector);
+
+    const ref = useRef()
+
+    useEffect(() => {
+        ref.current?.scrollIntoView({ behavior: "smooth" });
+      }, [message]);
+
+    return (
+    <div
+      ref={ref}
+      className={`message ${message.senderId === userDetails.uid && "owner"}`}
+    >
+      <div className="messageInfo">
+        <img
+          src={
+            message.senderId === userDetails.uid
+              ? userDetails.photoURL
+              : user.photoURL
+          }
+          alt="user"
+        />
+        <span>just now</span>
+      </div>
+      <div className="messageContent">
+        <p>{message.text}</p>
+        {message.img && <img src={message.img} alt="you" />}
+      </div>
+    </div>
+  );
 }
--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -1,9 +1,33 @@
+import { useSelector } from "react-redux"
 import "./Messages.css"
+import { chatSelector } from "../../redux/Reducers/chatReducer";
+import { useEffect, useState } from "react";
+import { doc, onSnapshot } from "firebase/firestore";
+import { db } from "../../config/firebase";
+import Message from "../Message/Message";
 
-export default function Messages(){
-    return(
-        <div className="MessagesContainer">
-            MessagesContainer
+export default function Messages() {
+
+    const { chatId } = useSelector(chatSelector);
+
+    const [messages, setMessages] = useState([]);
+
+    useEffect(()=>{
+        const unsub = onSnapshot(doc(db, "chats", chatId), (doc) => {
+            doc.exists() && setMessages(doc.data().messages);
+        })
+
+        return ()=>{
+            unsub();
+        }
+    }, [chatId])
+
+
+    return (
+        <div className="messages">
+            {messages.map((m) => (
+                <Message message={m} key={m.id} />
+            ))}
         </div>
-    )
+    );
 }
